a7fee9c0167ba1827ec7010a4636fd8ba267bdfb
MartinMiklos123 <136020503+MartinMiklos123@users.noreply.github.com>
2023-09-18 17:37:43 +0200

    Add files via upload

---
+++ uloha_8.py
@@ -0,0 +1,9 @@
+pocet_rokov = 2023 - 2009
+pocet_mesiacov = 8
+
+print("pocet dni: %i" % (pocet_rokov*365+pocet_mesiacov*30))
+print("pocet hodin: %i" % ((pocet_rokov*365+pocet_mesiacov*30) * 24))
+print("pocet sekund: %i" % ((pocet_rokov*365+pocet_mesiacov*30) * 24 * 60 * 60))
+
+
+
